00783986960707d3f0fdc3255306b06563c86651
williamsHack <williamyawdickson1998@gmail.com>
2023-03-22 16:33:42 -0700

    void unused variables and remove return statement

--- m_pop.c
+++ m_pop.c
@@ -1,29 +1,24 @@
 #include "monty.h"
 
 /**
- * m_pop - pop top element off of `stack'
- * @stack: double pointer to head of stack
- * @line_number: line number of current operation
+ * pop - removes the top element of the stack.
+ * @stack: double pointer to the head of the stack
+ * @line_number: the number of the line in file
  *
  * Return: void
  */
-void m_pop(stack_t **stack, unsigned int line_number)
+void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *pop = *stack;
+	stack_t *temp;
 
-	if (var.stack_len == 0)
+	if (stack == NULL || *stack == NULL)
 	{
-		dprintf(STDOUT_FILENO,
-			"L%u: can't pop an empty stack\n",
-			line_number);
+		printf("L%u: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	(*stack)->next->prev = (*stack)->prev;
-	(*stack)->prev->next = (*stack)->next;
-	if (var.stack_len != 1)
-		*stack = (*stack)->next;
-	else
-		*stack = NULL;
-	free(pop);
-	var.stack_len--;
+
+	temp = *stack;
+	*stack = (*stack)->next;
+
+	free(temp);
 }
