7de91718c0ab38b10142963294f8d2991740c777
williamsHack <williamyawdickson1998@gmail.com>
2023-03-22 16:42:04 -0700

    Avoid extra pops

--- mod.c
+++ mod.c
@@ -1,41 +1,10 @@
-#include "monty.h"
-
-/**
- * mod - computes the rest of the division of the second top element
- * of the stack by the top element of the stack.
- * @stack: double pointer to the head of the stack
- * @line_number: the number of the line in file
- *
- * Return: void
- */
-void mod(stack_t **stack, unsigned int line_number)
-{
-	stack_t *temp;
-	int product;
-
-	if (!stack || !(*stack) || !(*stack)->next)
-	{
-		printf("L%u: can't mod, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	temp = *stack;
-
-	product = temp->next->n % temp->n;
-	temp->next->n = product;
-
-	*stack = temp->next;
-
-	free(temp);
-}
-
 /**
  * m_mod - compute remainder (modulus) of second element divided by top element
  * @stack: double pointer to head of stack
  * @line_number: line number of current operation
  *
  * Return: void
- 
+ */ 
 void m_mod(stack_t **stack, unsigned int line_number)
 {
 	int n;
@@ -57,4 +26,4 @@ void m_mod(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	(*stack)->n %= n;
-}*/
+}
