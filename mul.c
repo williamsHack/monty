d6b008fe3b88d60f9b2ab58f086a0c8744cc272a
williamsHack <williamyawdickson1998@gmail.com>
2023-03-22 17:13:20 -0700

    fixed

--- mul.c
+++ mul.c
@@ -5,23 +5,25 @@
 #include "monty.h"
 
 /**
- * _add -  adds the first two nodes of the stack
+ * _mul - divides the next top value by the top value
  * @stack: stack given by main
  * @line_cnt: line counter
  *
  * Return: void
  */
-void _add(stack_t **stack, unsigned int line_cnt)
+void _mul(stack_t **stack, unsigned int line_cnt)
 {
 	int result;
 
 	if (!stack || !*stack || !((*stack)->next))
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_cnt);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_cnt);
 		exit(EXIT_FAILURE);
+		return;
 	}
 
-	result = ((*stack)->next->n) + ((*stack)->n);
-	pop(stack, line_cnt); /*For top node*/
+	result = ((*stack)->next->n) * ((*stack)->n);
+	pop(stack, line_cnt);/*For top node*/
 	(*stack)->n = result;
 }
+
